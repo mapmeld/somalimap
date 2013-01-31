62669d44888f25d8186fbf76844b5e26d4d4458e
Nick Doiron <ndoiron@mapmeld.com>
2013-01-31 20:01:10 +1200

    improve polygon point order

--- scripts/displaymap.js
+++ scripts/displaymap.js
@@ -1,7 +1,8 @@
 var DEBUG = false;
 var firstfile = true;
 
-var map;
+var map, anim, adjustAnim;
+var tick = null;
 
 $(document).ready(function(){
   // make a Leaflet map
@@ -17,11 +18,11 @@ $(document).ready(function(){
   $("#slidebar").slider({
     orientation: "horizontal",
     range: "min",
-    min: (new Date()) - 24 * 60 * 60 * 1000,
-    max: (new Date()) * 1,
-    value: 500,
+    min: 0,
+    max: 100,
+    value: 0,
     slide: function(event, ui){
-      displayTime(ui.value);
+      adjustAnim(ui.value);
     }
   });
   
@@ -38,94 +39,167 @@ $(document).ready(function(){
     }
     $.getJSON("content1.geojson", function(frame1){
       var animcoords = [ ];
-      for(var p=0;p<Math.min( layers.length, frame1.features.length );p++){
+      //for(var p=0;p<Math.min( layers.length, frame1.features.length );p++){
+      for(var p=0;p<frame0.features.length;p++){
         var oldcoords = layers[p];
-        var newcoords = frame1.features[p].geometry.coordinates[0];
         // remove repeat coordinates
         if(oldcoords[0].lat == oldcoords[oldcoords.length-1].lat && oldcoords[0].lng == oldcoords[oldcoords.length-1].lng){
           oldcoords.pop();
         }
-        if(newcoords[0][1] == newcoords[newcoords.length-1][1] && newcoords[0][0] == newcoords[newcoords.length-1][0]){
-          newcoords.pop();
-        }
-        for(var n=0;n<newcoords.length;n++){
-          var nearest = { index: -1, distance: 360 };
+        // if there is no match in the next frame, shrink this to nothing
+        if(p >= frame1.features.length){
           for(var i=0;i<oldcoords.length;i++){
-            var dist = Math.pow( Math.pow(oldcoords[i].lat - newcoords[n][1], 2) + Math.pow(oldcoords[i].lng - newcoords[n][0], 2), 0.5);
-            if(dist < nearest.distance){
-              nearest = { index: i, distance: dist };
+            oldcoords[i] = { lat: oldcoords[i].lat, lng: oldcoords[i].lng };
+          }
+        }
+        else{
+
+          var newcoords = frame1.features[p].geometry.coordinates[0];
+          // remove repeat coordinates
+          if(newcoords[0][1] == newcoords[newcoords.length-1][1] && newcoords[0][0] == newcoords[newcoords.length-1][0]){
+            newcoords.pop();
+          }
+          for(var n=0;n<newcoords.length;n++){
+            var nearest = { index: -1, distance: 360 };
+            for(var i=0;i<oldcoords.length;i++){
+              var dist = Math.pow( Math.pow(oldcoords[i].lat - newcoords[n][1], 2) + Math.pow(oldcoords[i].lng - newcoords[n][0], 2), 0.5);
+              if(dist < nearest.distance){
+                nearest = { index: i, distance: dist };
+              }
             }
-          }
-          if(oldcoords[nearest.index].match == -1){
-            // first coordinate to match this existing coordinate
-            oldcoords[nearest.index].match = n;
-          }
-          else{
-            // add a repeat coordinate to move to this new coordinate
-            var prevcoords = oldcoords.concat(-1).splice(0, nearest.index + 1);
-            var repeat = { lat: oldcoords[nearest.index].lat, lng: oldcoords[nearest.index].lng, match: n };
-            prevcoords = prevcoords.concat( repeat );
-            oldcoords = prevcoords.concat( oldcoords.slice( nearest.index + 1 ) );
+            if(oldcoords[nearest.index].match == -1){
+              // first coordinate to match this existing coordinate
+              oldcoords[nearest.index].match = n;
+            }
+            else{
+              // add a repeat coordinate to move to this new coordinate
+              var prevcoords = oldcoords.concat(-1).splice(0, nearest.index);
+              var repeat = { lat: oldcoords[nearest.index].lat, lng: oldcoords[nearest.index].lng, match: n };            
+              for(var i=nearest.index;i<oldcoords.length;i++){
+                if(oldcoords[i].lat != oldcoords[nearest.index].lat || oldcoords[i].lng != oldcoords[nearest.index].lng){
+                  // reached end of matching coordinates
+                  prevcoords = prevcoords.concat( repeat );
+                  break;
+                }
+                if(oldcoords[i].match < n){
+                  prevcoords = prevcoords.concat( { lat: oldcoords[i].lat, lng: oldcoords[i].lng, match: oldcoords[i].match } );
+                }
+                else{
+                  prevcoords = prevcoords.concat( repeat );
+                  break;
+                }
+              }
+              oldcoords = prevcoords.concat( oldcoords.slice( prevcoords.length - 1 ) );
+              //console.log(oldcoords);
+            }
           }
         }
         var latlngs = [ ];
         for(var r=0;r<oldcoords.length;r++){
+          oldcoords[r].start_index = r;
           latlngs.push( new L.LatLng( oldcoords[r].lat, oldcoords[r].lng ) );
         }
         layers[p] = new L.polygon( latlngs );
         map.addLayer(layers[p]);
         animcoords.push( oldcoords );
       }
-      var anim = 1;
-      var tick = setInterval(function(){
+      anim = 1;
+
+      adjustAnim = function(count){
+        if(typeof count == 'undefined'){
+          count = null;
+        }
+        else{
+          anim = count;
+          if(tick){
+            clearInterval(tick);
+            tick = null;
+          }
+        }
         for(var p=0;p<animcoords.length;p++){
           var coords = animcoords[p];
-          var newcoords = frame1.features[p].geometry.coordinates[0];
+          if(anim >= 50){
+            coords.sort(function(a,b){ return b.match - a.match; });
+          }
+          else{
+            coords.sort(function(a,b){ return b.start_index - a.start_index; });          
+          }
           var latlngs = [ ];
-          var toLeft = function(c){
-            if(c == 0){
-              c = coords.length;
-            }
-            if(coords[c-1].match > -1){
-              return new L.LatLng( newcoords[coords[c-1].match][1], newcoords[coords[c-1].match][0] );
-            }
-            else{
-              return toLeft(c-1);
-            }
-          };
-          var toRight = function(c){
-            if(c == coords.length - 1){
-              c = -1;
-            }
-            if(coords[c+1].match > -1){
-              return new L.LatLng( newcoords[coords[c+1].match][1], newcoords[coords[c+1].match][0] );
-            }
-            else{
-              return toRight(c+1);
-            }
-          };
-          for(var c=0;c<coords.length;c++){
-            if(coords[c].match > -1){
-              // coordinate has a destination
-              var animlat = coords[c].lat + (newcoords[coords[c].match][1] - coords[c].lat) * anim / 100;
-              var animlng = coords[c].lng + (newcoords[coords[c].match][0] - coords[c].lng) * anim / 100;
-              latlngs.push( new L.LatLng(animlat, animlng) );
-            }
-            else{
-              // coordinate has no destination, slip between next-door coordinates
-              var animlat = coords[c].lat + ((toLeft(c).lat + toRight(c).lat) / 2 - coords[c].lat) * anim / 100;
-              var animlng = coords[c].lng + ((toLeft(c).lng + toRight(c).lng) / 2 - coords[c].lng) * anim / 100;              
-              latlngs.push( new L.LatLng(animlat, animlng) );
+          if(p < frame1.features.length){
+            // matching polygon exists
+            var newcoords = frame1.features[p].geometry.coordinates[0];
+            var toLeft = function(c){
+              if(c == 0){
+                c = coords.length;
+              }
+              if(coords[c-1].match > -1){
+                return new L.LatLng( newcoords[coords[c-1].match][1], newcoords[coords[c-1].match][0] );
+              }
+              else{
+                return toLeft(c-1);
+              }
+            };
+            var toRight = function(c){
+              if(c == coords.length - 1){
+                c = -1;
+              }
+              if(coords[c+1].match > -1){
+                return new L.LatLng( newcoords[coords[c+1].match][1], newcoords[coords[c+1].match][0] );
+              }
+              else{
+                return toRight(c+1);
+              }
+            };
+            for(var c=0;c<coords.length;c++){
+              if(coords[c].match > -1){
+                // coordinate has a destination
+                var animlat = coords[c].lat + (newcoords[coords[c].match][1] - coords[c].lat) * anim / 100;
+                var animlng = coords[c].lng + (newcoords[coords[c].match][0] - coords[c].lng) * anim / 100;
+                latlngs.push( new L.LatLng(animlat, animlng) );
+              }
+              else{
+                // coordinate has no destination, slip between next-door coordinates
+                var animlat = coords[c].lat + ((toLeft(c).lat + toRight(c).lat) / 2 - coords[c].lat) * anim / 100;
+                var animlng = coords[c].lng + ((toLeft(c).lng + toRight(c).lng) / 2 - coords[c].lng) * anim / 100;              
+                latlngs.push( new L.LatLng(animlat, animlng) );
+              }
             }
           }
-
+          else{
+            // disappearing polygon
+            if(anim == 100){
+              map.removeLayer(layers[p]);
+            }
+            else{
+              if(!map.hasLayer(layers[p])){
+                map.addLayer(layers[p]);
+              }
+              if(typeof layers[p].avglat == 'undefined'){
+                var avglat = 0;
+                var avglng = 0;
+                for(var c=0;c<coords.length;c++){
+                  avglat += coords[c].lat;
+                  avglng += coords[c].lng;
+                }
+                avglat /= coords.length;
+                avglng /= coords.length;
+                layers[p].avglat = avglat;
+                layers[p].avglng = avglng;
+              }
+              for(var c=0;c<coords.length;c++){
+                latlngs.push( new L.LatLng( coords[c].lat + (layers[p].avglat - coords[c].lat) * anim / 100 , coords[c].lng + (layers[p].avglng - coords[c].lng) * anim / 100 ) );
+              }
+            }
+          }
           layers[p].setLatLngs( latlngs );
         }
         anim++;
         if(anim == 101){
           clearInterval(tick);
         }
-      }, 30);
+      };
+
+      tick = setInterval(adjustAnim, 30);
     });
   });
 });
